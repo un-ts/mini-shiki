e95162b6241019658e5ac124ae84620ee7686819
ocavue <ocavue@gmail.com>
2025-03-20 11:14:14 +1100

    Don't bundle @shikijs/langs and @shikijs/themes

--- scripts/update_shiki.sh
+++ scripts/update_shiki.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Install the latest version
-npm i shiki@latest @shikijs/engine-oniguruma@latest @shikijs/types@latest @shikijs/vscode-textmate@latest
+npm i shiki@latest @shikijs/engine-oniguruma@latest @shikijs/types@latest @shikijs/vscode-textmate@latest @shikijs/langs@latest @shikijs/themes@latest
 
 # Build
 npm run build
